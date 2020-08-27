b2f346ca19dd82585946d6e210626aac51d83521
Mai Tuyen <maituyen4496@gmail.com>
2020-08-27 14:38:35 +0700

    create base repo
    Signed-off-by: Mai Tuyen <maituyen4496@gmail.com>

---
+++ MyCover/MyCover.Service/Repository/BaseRepo.cs
@@ -0,0 +1,43 @@
+﻿using MyCover.Model.Entities;
+using MyCover.Service.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyCover.Service.Repository
+{
+    public class BaseRepo<TEntity> : IBaseRepo<TEntity> where TEntity : class
+    {
+        private readonly MyCoverDbContext context;
+        public BaseRepo(MyCoverDbContext context)
+        {
+            this.context = context;
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
